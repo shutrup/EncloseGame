2940a1d3a5218d5a4bb2f73fe2590155f5fcfed4
shutrup <magagaraev95@mail.ru>
2026-02-04 16:50:02 +0300

    Initial Commit

---
+++ Enclose/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Enclose
+//
+//  Created by Шарап Бамматов on 04.02.2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Enclose/EncloseApp.swift
@@ -0,0 +1,17 @@
+//
+//  EncloseApp.swift
+//  Enclose
+//
+//  Created by Шарап Бамматов on 04.02.2026.
+//
+
+import SwiftUI
+
+@main
+struct EncloseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
